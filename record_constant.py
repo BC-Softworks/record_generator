21edd8dae84ce9332f8e6dfaa19b12bb84438890
superseeker13 <cardseller2@gmail.com>
2021-05-25 21:12:38 -0400

    Create record_constant.py

---
+++ record_constant.py
@@ -0,0 +1,23 @@
+# Define variables
+import cmath.tau
+from functools import reduce
+
+samplingRate = 44100 # 44.1khz audio
+rpm = 45
+downsampling = 4
+thetaIter = (60 * samplingRate) / (downsampling * rpm)
+diameter = 7 # diameter of record in inches
+radius = diameter / 2 # radius of record inches
+innerHole = 143/500 # diameter of center hole in inches
+innerRad = 47/20 # radius of innermost groove in inches
+outerRad = 23/4 # radius of outermost groove in inches
+recordHeight = 1/25
+micronsPerInch = 25400
+micronsPerLayer = 16 # microns per vertical print layer
+amplitude = (24 * micronsPerLayer) / micronsPerInch # 24 is the amplitude of signal (in 16 micron steps)
+depth = (* 6 micronsPerLayer) / micronsPerInch # 6 is the measured in 16 microns steps, depth of tops of wave in groove from uppermost surface of record
+bevel = 0.5 # bevelled groove edge
+grooveWidth = 1/300 # in 600dpi pixels
+incrNum = tau / thetaIter # calculcate angular incrementation amount
+radIncr = (grooveWidth + 2 * bevel * amplitude) / thetaIter  # calculate radial incrementation amount
+rateDivisor = 0 # Not sure what this should be yet
