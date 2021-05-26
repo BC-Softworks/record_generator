0023e7cfed2a7ef06c041884249cc00636398ae8
superseeker13 <cardseller2@gmail.com>
2021-05-26 16:40:06 -0400

    Update rg.py

--- rg.py
+++ rg.py
@@ -1,22 +1,22 @@
 import cmath.tau
 import numpy as np
-import OpenGL
-import OpenGL.GL
-import OpenGL.GLUT
-import OpenGL.GLU
-import record_constant
+import math
+from stl import mesh
+from record_constant import *
 
 # Generate circumference of record
+# Recursive call comes first to to order of evaluation
 def generatecircumference(theta r):
-  return [[recordconstant.radius + r * sin(theta), recordconstant.radius + r * cos(theta)], generatecircumference(theta + recordconstant.incrNum, r)] if theta < cmath.tau else []
+  return generatecircumference(theta + incrNum, r) + [radius + r * sin(theta), radius + r * cos(theta)] if theta < cmath.tau else [radius, radius + r]
 
 #Add z position to each vector of x and y
 def setzpos(arr):
+  return (map(lambda lst : lst.append(recordHeight), arr), map(lambda lst: lst.append(0), arr))
 
 # Combine the vectors in to an outer and inner circle
 def calculaterecordshape():
-  outerEdge = setzpos(generatecircumference(0, record_constant.radius))
-  centerHole = setzpos(generatecircumference(0, record_constant.innerHole / 2))
+  outerEdge = setzpos(generatecircumference(0, radius))
+  centerHole = setzpos(generatecircumference(0, innerHole / 2))
 
 # Main function  
 def rg(filename):
@@ -25,7 +25,7 @@ def rg(filename):
   arr = np.asarray(open(filename, 'rt', newline='').read().partition(','), dtype='numpy.single')
   # Normalize the values
   arr = np.divide(arr, np.amax(arr))
-  totalGrooveNum = lens(normalizedDepth) / (record_constant.rateDivisor * record_constant.thetaIter)
+  totalGrooveNum = lens(normalizedDepth) / (rateDivisor * thetaIter)
   print("Grooves to draw {}.".format(totalGrooveNum))
   
   
