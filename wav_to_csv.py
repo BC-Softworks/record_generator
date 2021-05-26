74b373a0e332c4460e6e0867c3a91357f6f46e44
superseeker13 <cardseller2@gmail.com>
2021-05-25 21:28:54 -0400

    Update wav_to_csv.py

--- wav_to_csv.py
+++ wav_to_csv.py
@@ -1,2 +1,18 @@
 import wave
+import csv
 import math
+import sys
+
+if sys.argv.length != 2 :
+  print('Wrong number of arguements.')
+  quit()
+
+if sys.argv[1].endswith('.wav') :
+  print('Not a wave file.')
+  quit()  
+
+filename = sys.argv[1]
+
+with wave.open(fileName, 'rb') as w_file:
+  with open(fileName[:-4] + '.csv', 'w', newline='') as csvfile:
+    
