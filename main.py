230dee9084fb4353ec90136b9cdc2cf3b1affdd6
superseeker13 <cardseller2@gmail.com>
2021-05-26 12:50:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,14 @@ import tkinter as tk
 # Create window
 window = tk.Tk()
 window.title('Record Generator')
-
+label = tk.Label(text="Wav file")
+entry = tk.Entry()
+label.pack()
+entry.pack()
+button = tk.Button(
+    text="Generate",
+    width=25,
+    height=5,
+)
+button.pack()
+window.mainloop()
