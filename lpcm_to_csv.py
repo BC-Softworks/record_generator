c675ba8b417844d6e97a2fdca7d8e7bed052db7b
superseeker13 <cardseller2@gmail.com>
2021-05-26 12:31:13 -0400

    Update lpcm_to_csv.py
    Normalized data and append to csv

--- lpcm_to_csv.py
+++ lpcm_to_csv.py
@@ -1,9 +1,7 @@
 # Used for converting multi and single channel uncompressed
 # audio to a single waveform for analog transcription.
 
-import aifc # https://docs.python.org/3/library/aifc.html
 import wave # https://docs.python.org/3/library/wave.html
-import csv  # https://docs.python.org/3/library/csv.html
 import numpy as np # https://numpy.org/
 import math
 import sys
@@ -11,7 +9,7 @@ import sys
 # Bit depths of 8, 16, and 32 are supported for conversion
 # However, a bit depth of 12 is the max range a standard record player
 
-supported_formats = ['wav', 'wave', 'aifc', 'aiff']
+supported_formats = ['wav', 'wave']
 
 if sys.argv.length != 2 :
   print('Wrong number of arguements.')
@@ -27,14 +25,23 @@ if extension not in supported_formats:
   print('Not a supported file format.')
   quit()
 
-def getbytesarray(λ):
-  with λ.open(fileName, 'rb') as file:
-    number_of_frames = λ.getnframes()
-    return np.frombuffer(file.readframes(numframes), np.byte)
   
-audio_arr = extension in ['wav', 'wave']: ? getbytesarray(wave) : getbytesarray(afic)
-
-with open(fileName.split(".")[0] + '.csv', 'w', newline='') as csvfile:
-      csv_writer = csv.writer(csvfile, delimiter=' ')  
-      
-          
+with wave.open(fileName, 'rb') as wav:
+  number_of_frames = wav.getnframes()
+  numch = wav.getnchannels()
+  depth = wav.getsampwidth()
+  bit_depth = depth*8;
+  auto_data = np.frombuffer(w.readframes(numframes), dtype=np.byte)
+  merged = np.zeros((numframes,), dtype=np.byte)
+  with open(fileName.split(".")[0] + '.csv', 'w', newline='') as csvfile:
+    for i in range(0, numch):
+      print ("Extracting {} / {} channels, {} depth".format(i+1, numch, bit_depth))
+      ch_data = auto_data[channel::nch]
+      merged = np.add(merged, ch_data)
+    # Normalized data and append to csv  
+    for i in range(0, numch)
+      if merged[i] > 2**(bit_depth - 1):
+        merged[i] = (merged[i]-2**bit_depth)
+      elif merged[i] == 2**(bit_depth - 1):
+        merged[i] = 0
+      csvfile.append(merged[i] + ',')
